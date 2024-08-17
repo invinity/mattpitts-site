2cbd538bede1fed1391d68258d677fde2ddab2e3
Matt Pitts <matt@gopitts.net>
2024-08-17 10:09:11 -0400

    stuff

--- src/app/service/linkedin.service.ts
+++ src/app/service/linkedin.service.ts
@@ -6,7 +6,7 @@ import { RestliClient, LIGetResponse, AuthClient } from 'linkedin-api-client';
 })
 export class LinkedinService {
   restliClient = new RestliClient();
-  authClient = new AuthClient({ clientId: '78orjd2aty5z2c', clientSecret: 'GjBAXM4xp4iCCBqL', redirectUrl: 'http://localhost:4200/' });
+  authClient = new AuthClient({ clientId: 'xxxxx', clientSecret: 'xxxxxxx', redirectUrl: 'http://localhost:4200/' });
 
   constructor() { }
 
