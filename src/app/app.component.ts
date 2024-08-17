03aacf7912458cbf924d65c977ac0327db49d622
Matt Pitts <matt@gopitts.net>
2024-08-17 10:08:20 -0400

    initial content/styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LinkedinService } from './service/linkedin.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mattpitts-site';
+  title = 'Meet Matt Pitts';
   linkedInAuthUrl: string;
 
   constructor(private  linkedin: LinkedinService) {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'resume', component: ResumeComponent},
+    {path: '', component: DashboardComponent}
+];
