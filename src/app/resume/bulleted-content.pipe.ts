78f4b9909752c76b7bbc55f562f10699e5545400
Matt Pitts <matt@gopitts.net>
2024-10-11 13:24:07 -0400

    resume stylings

--- src/app/resume/bulleted-content.pipe.ts
+++ src/app/resume/bulleted-content.pipe.ts
@@ -9,12 +9,15 @@ export class BulletedContentPipe implements PipeTransform {
   transform(value: string | undefined, ...args: unknown[]): unknown {
     if (value === undefined)
       return null
-    const list = value.split(/\*/)
+    const list = value.split(/\*\s+/)
     const ul = document.createElement("ul")
     list.forEach(i => {
-      const li = document.createElement("li")
-      li.innerHTML = i
-      ul.appendChild(li)
+      i = i.trim()
+      if (i !== "") {
+        const li = document.createElement("li")
+        li.innerHTML = i
+        ul.appendChild(li)
+      }
     });
     return ul.outerHTML
   }
