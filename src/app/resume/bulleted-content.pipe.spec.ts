0594449e49d139906b4e6c60843743a6a9aa266a
Matt Pitts <matt@gopitts.net>
2024-10-25 09:27:28 -0400

    Improve asterisks-to-bulleted-list conversion

--- src/app/resume/bulleted-content.pipe.spec.ts
+++ src/app/resume/bulleted-content.pipe.spec.ts
@@ -5,4 +5,21 @@ describe('BulletedContentPipe', () => {
     const pipe = new BulletedContentPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('converts only line-delimited and bulleted content', () => {
+    const pipe = new BulletedContentPipe();
+    const output = pipe.convertToSpan(`
+      This is a dummy description raw value.
+      * It has an initial summary text above
+      * And then this list of bulleted items
+      * Which should be converted into individual <li>
+`)
+    expect(output).toBeInstanceOf(HTMLSpanElement)
+    expect(output?.children).toHaveSize(1)
+    expect(output?.innerText.trim()).toMatch(/^This is a dummy description raw value./)
+    expect(output?.children[0]).toBeInstanceOf(HTMLUListElement)
+    expect(output?.children[0].children).toHaveSize(3)
+    expect(output?.children[0].children[0].innerHTML.trim()).toEqual("It has an initial summary text above")
+    expect(output?.children[0].children[2].innerHTML.trim()).toEqual("Which should be converted into individual &lt;li&gt;")
+  });
 });
--- src/app/resume/bulleted-content.pipe.ts
+++ src/app/resume/bulleted-content.pipe.ts
@@ -7,19 +7,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class BulletedContentPipe implements PipeTransform {
 
   transform(value: string | undefined, ...args: unknown[]): unknown {
-    if (value === undefined)
-      return null
-    const list = value.split(/\*\s+/)
-    const ul = document.createElement("ul")
-    list.forEach(i => {
-      i = i.trim()
-      if (i !== "") {
-        const li = document.createElement("li")
-        li.innerHTML = i
-        ul.appendChild(li)
-      }
-    });
-    return ul.outerHTML
+    return this.convertToSpan(value)?.outerHTML
   }
 
+  convertToSpan(value: string | undefined): HTMLSpanElement | undefined {
+    if (value === undefined)
+      return undefined
+    value = value.trim()
+    const content = document.createElement("span")
+    const ul = document.createElement("ul")
+    var currentItem: HTMLLIElement | undefined = undefined
+    chars: for (let index = 0; index < value.length; index++) {
+      const char = value[index];
+      if (char === "*") {
+        currentItem = document.createElement("li")
+        ul.appendChild(currentItem)
+      } else {
+        if (currentItem) {
+          currentItem.innerText += char
+        } else {
+          content.innerText += char
+        }
+      }
+    }
+    content.appendChild(ul)
+    return content
+  }
 }
