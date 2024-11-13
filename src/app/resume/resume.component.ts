cd326c05b88b71e5de18b37270a8e85316b7dc35
Matt Pitts <matt@gopitts.net>
2024-11-13 08:58:13 -0500

    add the ability to remove and reorder some elements of the resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -5,17 +5,22 @@ import { LINKEDIN_SERVICE, LinkedinService } from '../service/linkedin.service';
 import { ElegantComponent } from './template/elegant/elegant.component';
 import { TimelineComponent } from "./template/timeline/timeline.component";
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [ElegantComponent, MatProgressBarModule, TimelineComponent, MatButtonModule],
+  imports: [ElegantComponent, MatProgressBarModule, TimelineComponent, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent implements OnInit {
   profile: any | undefined
   profileLoaded = false
+  selectedSpecialization: string | undefined
 
   constructor(@Inject(LINKEDIN_SERVICE) private linkedin: LinkedinService, private snackBar: MatSnackBar) {
   }
--- src/app/resume/template/timeline/timeline.component.ts
+++ src/app/resume/template/timeline/timeline.component.ts
@@ -2,15 +2,55 @@ import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { DescriptionComponent } from './description/description.component';
 import { NgFor } from '@angular/common';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'resume-timeline',
   standalone: true,
-  imports: [MatIcon, DescriptionComponent, NgFor],
+  imports: [MatIcon, DescriptionComponent, NgFor, CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
 export class TimelineComponent {
   @Input()
   profile: any | undefined
+
+  removedProjects = new Array<Element>()
+
+  skills = [
+    "Java",
+    "SOA & REST",
+    "Full-stack development",
+    "RDBMS",
+    "Kafka",
+    "Reactive Programming",
+    "CI/CD",
+    "Software Design & Testing",
+    "SDLC tools and practices",
+    "Team lead & Technical coach",
+    "Microservices",
+    "AWS",
+    "TCP/IP Socket Communications",
+    "Golang",
+    "Docker"
+  ]
+
+  removedSkills = new Array<string>()
+
+  removeSkill(skill: string) {
+    const idx = this.skills.indexOf(skill)
+    transferArrayItem(this.skills, this.removedSkills, idx, idx)
+  }
+
+  removeProject(project: any) {
+    const idx = this.profile?.Projects.indexOf(project)
+    transferArrayItem(this.profile?.Projects, this.removedProjects, idx, idx)
+  }
 }
